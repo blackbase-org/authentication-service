721072b55ad1339061f01840126ffcfccf319130
Kode Sonik <95509615+kodesonik@users.noreply.github.com>
2024-07-22 23:52:09 +0000

    Allow cors

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: '*',
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
     credentials: true,
   });
 
